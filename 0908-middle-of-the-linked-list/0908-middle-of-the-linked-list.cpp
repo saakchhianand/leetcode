ca94e7be02f48da30567b0d8b8684498040800ce
saakchhianand <anandsaakchhi@gmail.com>
2025-08-25 12:11:47 +0530

    Time: 0 ms (100%), Space: 10 MB (25.38%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -13,10 +13,12 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* first = head;
         ListNode* second = head;
-        while(second != NULL && second -> next !=NULL){
-            first = first-> next;
-            second = second->next-> next;
+
+        while (second != nullptr && second->next != nullptr) {
+            first = first->next;
+            second = second->next->next;
         }
+        
         return first;
     }
 };
